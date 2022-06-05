c2a4b361626b91ada3828828b079402c90dc3575
graagm <graagm@gmail.com>
2022-06-05 07:02:00 +0300

    string name ex

--- Less1/Program.cs
+++ Less1/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿int x = 57;
+int z = 87;
+Console.WriteLine( x + z );
+string name1 = Console.ReadLine();
+string name2 = Console.ReadLine();
+if (name2 == ""){
+    Console.WriteLine("Ура");
+}
