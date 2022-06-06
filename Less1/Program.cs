00d4e6074ee4df1eff959cb5205ede0222668fcf
graagm <graagm@gmail.com>
2022-06-06 09:01:25 +0300

    massive Random add

--- Less1/Program.cs
+++ Less1/Program.cs
@@ -1,8 +1,16 @@
-﻿int x = 57;
-int z = 87;
-Console.WriteLine( x + z );
-string name1 = Console.ReadLine();
-string name2 = Console.ReadLine();
-if (name2 == ""){
-    Console.WriteLine("Ура");
+﻿// int x = 57;
+// int z = 87;
+// Console.WriteLine( x + z );
+// string name1 = Console.ReadLine();
+// string name2 = Console.ReadLine();
+// if (name2 == ""){
+//     Console.WriteLine("Ура");
+// }
+int N = 10;
+int [] c = new int [N];
+for (int i=0; i<N; i++)
+{
+    c[i] = new Random().Next(15, 39);
+    Console.Write(c[i]);
+    Console.Write(' ');
 }
