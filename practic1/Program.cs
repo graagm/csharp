a1a5642305bb4d9ebff6e4c7128f23bf2502eb7e
graagm <graagm@gmail.com>
2022-06-06 11:07:02 +0300

    week add

--- practic1/Program.cs
+++ practic1/Program.cs
@@ -8,3 +8,48 @@
 // Console.WriteLine(x * x);
 
 // Задача2
+// int x, y;
+// Console.WriteLine("Введите 2 числа для проверки является ли втрое квадратом первого");
+// x = int.Parse(Console.ReadLine());
+// y = int.Parse(Console.ReadLine());
+// if (Math.Pow(x,2) == y) 
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+// Задача3
+Console.WriteLine("введите день недели от 1 до 7");
+string day; 
+int num = int.Parse(Console.ReadLine());
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
