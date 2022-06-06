e429f893d05d9093e14f586797890309dd140838
graagm <graagm@gmail.com>
2022-06-06 12:03:15 +0300

    with task 7

--- practic1/Program.cs
+++ practic1/Program.cs
@@ -22,34 +22,49 @@
 // }
 
 // Задача3
-Console.WriteLine("введите день недели от 1 до 7");
-string day; 
-int num = int.Parse(Console.ReadLine());
-if (num == 1)
-{
-    Console.WriteLine("Понедельник");
-}
-else if (num == 2)
-{
-    Console.WriteLine("Вторник");
-}
-else if (num == 3)
-{
-    Console.WriteLine("Среда");
-}
-else if (num == 4)
-{
-    Console.WriteLine("Четверг");
-}
-else if (num == 5)
-{
-    Console.WriteLine("Пятница");
-}
-else if (num == 6)
-{
-    Console.WriteLine("Суббота");
-}
-else if (num == 7)
-{
-    Console.WriteLine("Воскресенье");
-}
+// Console.WriteLine("введите день недели от 1 до 7");
+// string day; 
+// int num = int.Parse(Console.ReadLine());
+// if (num == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (num == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (num == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (num == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (num == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (num == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (num == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+
+// Задача5
+// Console.WriteLine("Введите целое число");
+// int x;
+// x = int.Parse(Console.ReadLine());
+// for (int i=-x; i<=x; i++)
+// {
+//     Console.Write(i + ", ");
+// }
+
+
+// Задача7
+Console.WriteLine("Введите трехзначное число");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine(x % 10);
