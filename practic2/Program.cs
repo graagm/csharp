88dab58e4da3950ac9d47cb52b06755cc522f726
graagm <graagm@gmail.com>
2022-06-09 12:51:00 +0300

    end of practoc2

--- practic2/Program.cs
+++ practic2/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿// Задача 1
+
+// Console.WriteLine("Введите трехзначное число");
+// int x = int.Parse(Console.ReadLine());
+// x = x % 10 + (x / 100) * 10;
+// Console.WriteLine($"после обрезки получилось число {x}");
+
+// Задача 2
+
+// Console.WriteLine("Введите 2 целых числа");
+// int x = int.Parse(Console.ReadLine());
+// int y = int.Parse(Console.ReadLine());
+// int z = x % y;
+// if (z == 0)
+// {
+//     Console.WriteLine("кратно");
+// }
+// else
+// {
+//     Console.WriteLine($"не кратно, остаток {z}");
+// }
+
+// Задача 3
+
+Console.WriteLine("Введите целое число и мы проверим делится ли оно оновременно на 7 и на 23");
+int x = int.Parse(Console.ReadLine());
+if ((x % 7 == 0) & (x % 23 == 0))
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
