be0e82b3981425447ce6075a7f5d306450f0c928
graagm <graagm@gmail.com>
2022-06-14 05:36:59 +0300

    practic3 start

---
+++ practic3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Console.WriteLine("введите номер четверти");
+// int P = int.Parse(Console.ReadLine());
+// if (P == 1)
+// {
+//     Console.WriteLine("x > 0 и y > 0");
+// }
+// else if (P == 2)
+// {
+//      Console.WriteLine("x < 0 и y > 0");   
+// }
+// else if (P == 3)
+// {
+//      Console.WriteLine("x < 0 и y < 0");   
+// }else if (P == 4)
+// {
+//      Console.WriteLine("x > 0 и y < 0");   
+// }
+
