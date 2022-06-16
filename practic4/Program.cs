08992ebe1228559076658689aaadabdcd49be21c
graagm <graagm@gmail.com>
2022-06-16 10:30:40 +0300

    practic4

---
+++ practic4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 24
+
+// Console.WriteLine("введите целое число и мы посчитаем сумму от 0");
+// int x = int.Parse(Console.ReadLine());
+// int s = 0;
+// for (int i=0; i<=x; i++)
+// {
+//     s += i;
+// }
+// Console.WriteLine($"сумма равна {s}");
+
+// Задача 26
+
+// Console.WriteLine("введите целое число и мы посчитаем количество его знаков");
+// int x = int.Parse(Console.ReadLine());
+// int s = 0;
+// string str = x.ToString();
+// s = str.Length;
+// Console.WriteLine($"количество знаков {s}");
+
+// Задача 28
+
+// Console.WriteLine("введите целое число и мы посчитаем факториал");
+// int x = int.Parse(Console.ReadLine());
+// int s = 1;
+// for (int i=1;i<=x;i++)
+// {
+//     s *= i;
+// }
+
+// Console.WriteLine($"факториал получился {s}");
+
+// Задача 30
+// Console.WriteLine("заполним массив случаынйми единицами и нулями");
+// int N = 8;
+// int [] ms = new int[N];
+// for (int i=0;i<N;i++)
+// {
+//     ms[i] = new Random().Next(0, 2);
+//     Console.Write($" {ms[i]}");
+// }
