f16bb53e41a7314d79896098a07f6778d2d34304
graagm <graagm@gmail.com>
2022-06-30 15:50:53 +0300

    Pascal added

--- practic8/Program.cs
+++ practic8/Program.cs
@@ -141,3 +141,104 @@
 // Console.WriteLine();
 // foreach (var x in ms1.GroupBy(p => p))
 // 	Console.WriteLine("{0} => {1}", x.Key, x.Count());
+
+
+// Задача 59
+
+
+// void msStart (int [,] ms)
+// {
+// 	for(int i=0; i<ms.GetLength(0);i++)
+//     {
+//         for(int j=0;j<ms.GetLength(1);j++)
+//         {
+// 	        ms[i, j] = new Random().Next(3, 9);
+// 	    }
+//     }
+// }
+
+// void msPrint(int [,]ms)
+// {
+// 	for(int i=0;i<ms.GetLength(0);i++)
+//     {
+//         for(int j=0;j<ms.GetLength(1);j++)
+//         {
+// 	        Console.Write($"{ms[i,j]} ");
+// 	    }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.WriteLine("введите величину массива");
+// int N = int.Parse(Console.ReadLine());
+// int M = int.Parse(Console.ReadLine());
+// int [,] ms = new int [N,M];
+// msStart(ms);
+// Console.WriteLine();
+// msPrint(ms);
+// int k = 0;
+// int l = 0;
+// int min = ms[k,l];
+// for (int i=0;i<N;i++)
+// {   
+//     for (int j=0;j<M;j++)
+//     {
+//         if ( ms[i,j] < ms[k,l]){
+//             (k, l) = (i, j);
+//         }
+//     }
+
+// }
+// Console.WriteLine();
+// for (int i=0;i<N;i++)
+// {   
+//     for (int j=0;j<M;j++)
+//     {
+//         if ( i != k & j != l){
+//             Console.Write($"{ms[i,j]} ");
+//         }
+//     }
+//     if ( i != k )
+//         {
+//             Console.WriteLine();
+//         }
+
+// }
+
+
+// Задача 61
+
+// Console.WriteLine("введите количество строк треугольника Паскаля");
+// int N = int.Parse(Console.ReadLine());
+// int [] ms1 = new int [1];
+// for(int i=0; i<N; i++)
+// {
+//     int M = i + 1;
+//     int [] ms = new int [M];
+
+//     (ms[0], ms[M - 1]) = (1, 1);
+//     if (i>1)
+//     {
+//         for(int s=1; s<= M/2; s++)
+//         {
+//             (ms[s], ms[M-1-s]) = (ms1[s-1]+ms1[s], ms1[s-1]+ms1[s]);
+//         }
+//     }
+//     int probel = N - M;
+//     for (int z=0; z<probel; z++)
+//     {
+//         Console.Write("  ");
+//     }
+//     for (int j=0; j<M; j++)
+//     {
+
+//         Console.Write($"{ms[j]}   ");
+//     }
+//     Console.WriteLine();
+//     Array.Resize(ref ms1, M);
+//     for (int j=0; j<M; j++)
+//     {
+//         ms1[j] = ms[j];
+//     }
+}
+
